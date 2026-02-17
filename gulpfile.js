59faee8bec8ea478aaa0b829f87b348218603901
lytvyniuk_v <lv@pinpools.com>
2026-02-17 19:09:51 +0100

    setted up project

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+import gulp from 'gulp';
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import cleanCSS from 'gulp-clean-css';
+import autoprefixer from 'gulp-autoprefixer';
+import uglify from 'gulp-uglify';
+import concat from 'gulp-concat';
+import imagemin from 'gulp-imagemin';
+import browserSync from 'browser-sync';
+import { deleteAsync } from 'del';
+
+const { src, dest, watch, series, parallel } = gulp;
+const sass = gulpSass(dartSass);
+const bs = browserSync.create();
+
+const paths = {
+  html: 'src/*.html',
+  styles: 'src/scss/**/*.scss',
+  scripts: 'src/js/**/*.js',
+  images: 'src/images/**/*',
+  dist: 'dist'
+};
+
+export function clean() {
+  return deleteAsync([paths.dist]);
+}
+
+export function html() {
+  return src(paths.html)
+    .pipe(dest(paths.dist))
+    .pipe(bs.stream());
+}
+
+export function styles() {
+  return src(paths.styles)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(cleanCSS())
+    .pipe(dest(`${paths.dist}/css`))
+    .pipe(bs.stream());
+}
+
+export function scripts() {
+  return src(paths.scripts)
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(dest(`${paths.dist}/js`))
+    .pipe(bs.stream());
+}
+
+export function images() {
+  return src(paths.images)
+    .pipe(imagemin())
+    .pipe(dest(`${paths.dist}/images`));
+}
+
+export function serve() {
+  bs.init({
+    server: { baseDir: paths.dist }
+  });
+
+  watch(paths.html, html);
+  watch(paths.styles, styles);
+  watch(paths.scripts, scripts);
+  watch(paths.images, images);
+}
+
+export default series(clean, parallel(html, styles, scripts, images), serve);
